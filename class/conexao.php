a1dbed5211d6c924576581e8936a0ac88a2ba485
rodriguesrenato61 <58270195+rodriguesrenato61@users.noreply.github.com>
2020-01-05 17:37:15 -0300

    configuração de conexão com o banco

--- class/conexao.php
+++ class/conexao.php
@@ -7,7 +7,7 @@
     $host = "localhost";
     $dbname = "escola";
     $user = "root";
-    $password = "d3s1p6g6";
+    $password = "";
 
     $sistema->conectar($host, $dbname, $user, $password);
 
