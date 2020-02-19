ccc35216fa52779299deb5aaacaeb510f752d28a
Renato Rodrigues de Souza Júnior <58270195+rodriguesrenato61@users.noreply.github.com>
2020-02-19 11:03:15 -0300

    conexão com o banco de dados

--- class/conexao.php
+++ class/conexao.php
@@ -7,7 +7,7 @@
     $host = "localhost";//host do banco
     $dbname = "escola";//nome do banco
     $user = "root";//usuário do banco
-    $password = "d3s1p6g6";//senha do banco
+    $password = "";//senha do banco
 
     $sistema->conectar($host, $dbname, $user, $password);//conectando com o banco
 
